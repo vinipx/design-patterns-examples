6f355e7e76413bf76e451b0f7a59163f9cd34ea8
vinipx <vinipeixotofagundes@gmail.com>
2022-12-25 13:29:33 +0100

    design-patterns - creational - singleton -

--- src/main/java/creational/singleton/example/EagerInitializedSingleton.java
+++ src/main/java/creational/singleton/example/EagerInitializedSingleton.java
@@ -1,2 +1,11 @@
-package creational.singleton.example;public class EagerInitializedSingleton {
+package creational.singleton.example;
+
+public class EagerInitializedSingleton {
+    private static EagerInitializedSingleton instance;
+    //private constructor for avoiding client applications for using constructor
+    private EagerInitializedSingleton(){}
+
+    public static EagerInitializedSingleton getInstance(){
+        return instance;
+    }
 }
